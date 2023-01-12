4059ed6dd14501c238d062aec98606d3196a2e3f
Eray Demirok <ben@eraydemirok.com>
2023-01-12 12:13:51 +0300

    fix: Androids

--- android/src/main/java/com/eraydemirok/react_native_card_swiper/CardSwiperView.java
+++ android/src/main/java/com/eraydemirok/react_native_card_swiper/CardSwiperView.java
@@ -1,9 +1,10 @@
 package com.eraydemirok.react_native_card_swiper;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.widget.CardView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+
 import android.util.AttributeSet;
 
 public class CardSwiperView extends CardView {
